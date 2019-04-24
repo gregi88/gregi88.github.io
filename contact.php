c9c247ebe587f4944b036b1eedbda6fc5243eb58
Grzegorz Stachowiak <gregista88@gmail.com>
2019-04-24 11:59:12 +0200

    edit form

--- contact.php
+++ contact.php
@@ -18,6 +18,6 @@ $headers = "From: $email_from \r\n";
 $headers .= "Reply-to: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
-header("Location: contact.html.erb");
+header("Location: contact.html");
 
 ?>
